037aa54bb3bf1d4178316877f8b9b86174c0e16f
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-11-16 17:56:16 +0100

    Add files via upload

---
+++ buricMarioPonavljanje3/buricMarioPonavljanje3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricMarioPonavljanje3
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            string str3 = Console.ReadLine();
+            Console.WriteLine("Unos prvog stringa: ");
+            Console.WriteLine("Unos drugog stringa: ");
+            Console.WriteLine("Unos trećeg stringa: ");
+            
+            string x = str1+str2+str3;
+            x = new string(x.Reverse().ToArray());
+            
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}
